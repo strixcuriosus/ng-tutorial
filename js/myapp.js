c1b73e5fcac86a38db94647d0c8d4571177e845c
Ash Hoover <strixcuriosus@gmail.com>
2015-02-25 15:00:52 -0800

    Use the http service to get a list of repositories from the github api

--- js/myapp.js
+++ js/myapp.js
@@ -4,6 +4,11 @@ app.run(function($rootScope){
     $rootScope.appName = 'Hello World';
 });
 
-app.controller('firstController', function($scope){
+app.controller('firstController', function($http, $scope){
     $scope.headerInfo = {'title': 'RepoList', 'count': 0};
+
+    $http.get('https://api.github.com/search/repositories?q=hello+world')
+    .success(function(data){
+        console.log(data);
+    });
 });
