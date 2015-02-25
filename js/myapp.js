7fda83bf795f5c043db3d21cdb85046e36970512
Ash Hoover <strixcuriosus@gmail.com>
2015-02-25 14:51:46 -0800

    Show scope inheritance

--- js/myapp.js
+++ js/myapp.js
@@ -3,3 +3,7 @@ var app = angular.module('myApp', []);
 app.run(function($rootScope){
     $rootScope.appName = 'Hello World';
 });
+
+app.controller('firstController', function($scope){
+    $scope.headerInfo = {'title': 'RepoList', 'count': 0};
+});
