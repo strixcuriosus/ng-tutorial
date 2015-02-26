aef37f1f2e4b97af8674d5b5c188baaf18767f87
Ash Hoover <strixcuriosus@gmail.com>
2015-02-25 16:11:37 -0800

    Add a stylesheet, implement repo liking

--- js/myapp.js
+++ js/myapp.js
@@ -7,8 +7,25 @@ app.run(function($rootScope){
 app.controller('firstController', function($http, $scope){
     $scope.headerInfo = {'title': 'RepoList', 'count': 0};
 
+    $scope.likeRepo = function(repo){
+        repo.liked = !repo.liked;
+    };
+
     $http.get('https://api.github.com/search/repositories?q=hello+world')
-    .success(function(data){
-        console.log(data);
-    });
+    .success(handleRepoList);
+
+    function handleRepoList(data){
+        $scope.repos = data.items.map(formatRepo).sort(function(a, b){
+            return b.stars - a.stars;
+        });
+    }
+
+    function formatRepo(repo){
+        return {'name': repo.name,
+                'description': repo.description,
+                'user': repo.owner.login,
+                'stars': parseInt(repo.stargazers_count, 10),
+                'liked': false
+            };
+    }
 });
