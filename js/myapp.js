f82161842ae139051add163b956e14587b1f8972
raphaelmerx <raphaelm@captricity.com>
2015-03-02 19:54:46 -0800

    Step 0 of the presentation

--- js/myapp.js
+++ js/myapp.js
@@ -1,10 +1,6 @@
 var app = angular.module('myApp', []);
 
 app.run(function($rootScope){
-    $rootScope.appName = 'Name this app!';
-});
-
-app.controller('firstController', function($http, $scope){
-    $scope.controllerVariable = 'I\'m the controller variable';
+    $rootScope.appName = 'Hello World';
 });
 
